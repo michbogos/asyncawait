a45c9f7216c65813f77cdce674dd4d0c461bcb91
michbogos <bogostamicheal@gmail.com>
2023-08-26 17:12:36 +0200

    Update repos

--- screenshot.py
+++ screenshot.py
@@ -13,9 +13,8 @@ import os
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
 options.add_argument("--headless")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_argument("--window-size=1920,1080")
-options.add_experimental_option('useAutomationExtension', False)
+options.binary_location = "/usr/bin/chromium"
 driver = webdriver.Chrome(
     options=options)
 
